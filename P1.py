2a52fbe4f5b09dc7052bda5d84ad8058c088b3e9
Anjali V B <anjalivbbala@gmail.com>
2024-09-19 17:25:05 +0530

    updated text to Dance

---
+++ P1.py
@@ -0,0 +1,18 @@
+#convert integer 3 to float and print its type
+x=3
+y=float(x)
+print (y)
+print (type(y))
+#To find the perimeter and area of the circle
+pi=355/113
+radius=2.2
+print(2*pi*radius)
+print(pi*radius**2)
+#"I can _ better than you" place a verb and print verb the 5 times
+text="Dance"
+print('I can',text,'better than you')
+print(text*5)
+
+
+
+
