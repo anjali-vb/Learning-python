a6500c47173679b0498d437c25207637f7f400e0
anjali-vb <anjalivbbala@gmail.com>
2024-09-20 12:25:46 +0530

    Add files via upload

---
+++ Add a verb to sentence.py
@@ -0,0 +1,4 @@
+#"I can _ better than you" place a verb and print verb the 5 times
+text="smile"
+print('I can',text,'better than you')
+print(text*5)
---
+++ Area and perimeter of a circle.py
@@ -0,0 +1,5 @@
+#To find the perimeter and area of the circle
+pi=355/113
+radius=2.2
+print(2*pi*radius)
+print(pi*radius**2)
