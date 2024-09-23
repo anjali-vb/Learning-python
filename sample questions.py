a8fc5a1964b8a2c035a69f136571467ba5191fe3
Anjali V B <anjalivbbala@gmail.com>
2024-09-23 12:32:41 +0530

    Removed head and unnecessary git details from a file

---
+++ sample questions.py
@@ -0,0 +1,18 @@
+#print numbers from 0 to 5
+for n in range(5):
+    print(n)
+#if start=6,end=4 find the sum using running sum method
+mysum = 0
+start = 3
+end = 5
+for i in range(start, end):
+    mysum += i
+print(mysum)
+
+mysum = 0
+start = 2
+end = 3
+for i in range(start, end):
+    mysum += i
+print(mysum)
+    
