bac3fb4fd685c0f4814af61504c9594661135029
Anjali V B <anjalivbbala@gmail.com>
2024-09-23 18:05:06 +0530

    added a new file to the group

--- sample questions.py
+++ sample questions.py
@@ -15,4 +15,15 @@ end = 3
 for i in range(start, end):
     mysum += i
 print(mysum)
+
+#code that loops over a range and prints howmany even numbers and odd numbers are in that range
+num=0
+for i in range(5):
+    i+=1
+    print(i)
+    if i%2==0:
+        print ("even number")
+    else:
+        print("odd number")
+
     
