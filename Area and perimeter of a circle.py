063942b46254191e9d2cfe05c429026c745a2e79
anjali-vb <anjalivbbala@gmail.com>
2024-09-23 12:56:40 +0530

    Update Area and perimeter of a circle.py

--- Area and perimeter of a circle.py
+++ Area and perimeter of a circle.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 #To find the perimeter and area of the circle
 pi=355/113
 radius=2.2
 print(2*pi*radius)
 print(pi*radius**2)
-=======
-#To find the perimeter and area of the circle
-pi=355/113
-radius=2.2
-print(2*pi*radius)
-print(pi*radius**2)
->>>>>>> a6500c47173679b0498d437c25207637f7f400e0
