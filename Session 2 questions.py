7bd434ee2ff57227b14682a230e0f64453e6bd8c
Anjali V B <anjalivbbala@gmail.com>
2024-09-24 14:08:15 +0530

    Deleted a duplicate file

---
+++ Session 2 questions.py
@@ -0,0 +1,97 @@
+#strings
+a="my"
+b="self"
+c=a+b
+print(c)
+#printing a+b with space between
+d=a+" "+b
+print(d)
+#print a three times
+silly=a*3
+print (silly)
+
+
+#b=":" ,c=b+2c then s1=b+2*c then s1?
+b=":"
+c="c"
+s1=b+2*c
+print(s1)
+
+
+#what is the length of a="abcdef"
+a="abcdef"
+print(len(a))#len()is a funtion used to find the length of a string
+
+#s="abc"
+s="abc"
+a=s[0]
+print(a)
+b=s[1]
+print(b)
+d=s[-1]
+print(d)
+e=s[-2]
+print(e)
+
+
+#slicing
+s="learningchampion"
+a=s[8:13]
+print(a)
+b=s[3:6]
+print(b)
+c=s[3:6:2]
+print(c)
+d=s[ : ]
+print(d)
+e=s[ : :-1]
+print(e)
+f=s[4:1:-2]
+print(f)
+g=s[8: ]
+print(g)
+
+
+#concatenate stringds together using + to get a single object ad print it 5 times with a gap inbetween
+a="The"
+b=3
+c="musketeers"
+d=a+str(b)+c
+print(d)
+print((d+" ")*5)
+
+
+#Samplequestion
+a=(2<3)
+print(a)
+
+
+#do equality and unequality test with any two integers
+a=5
+b=6
+print(a==b)
+print(a!=b)
+
+
+#sample question
+pset_time=15
+sleeping_time=5
+print(pset_time>sleeping_time)
+
+
+#sample question
+Derive=True
+Drink=False
+both=Derive and Drink
+print(both)
+
+
+#sample question
+x=not(2<3)
+print(x)
+y=(2<3)and(3<4)
+print(y)
+
+
+
+
