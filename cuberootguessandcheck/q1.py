8d3604c897cb9ce31218d6e1e703104e6dc19fb0
Anjali V B <anjalivbbala@gmail.com>
2024-09-25 18:34:08 +0530

    changed the contents of s file

--- cuberootguessandcheck/q1.py
+++ cuberootguessandcheck/q1.py
@@ -1,13 +1,11 @@
 #find the cube root of a number using guess and check method
 
-#for negative number
-cube=int(input("Enter an integer: "))
-for guess in range(abs(cube)+1):    #assume It as positive so weused absolute
-    if guess**3==abs(cube):
-        if cube<0:                     #to check to verify It is negative
-            guess=-guess            # here dealing with negative cube here
-            print('cube root of',cube,'is',guess)
-        
+#for a positive number
+cube=int(input("Enter a positive integer: "))
+for guess in range(cube+1):      #when the cube is 1 want to include the cube
+    if guess**3==cube:
+        print('cube root of',cube,'is',guess)
+
 
           
 
