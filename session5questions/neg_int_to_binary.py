68f1dbcefa41616a45a43378c8c3ee52e4b72672
Anjali V B <anjalivbbala@gmail.com>
2024-10-01 18:06:04 +0530

    modified a file

--- session5questions/neg_int_to_binary.py
+++ session5questions/neg_int_to_binary.py
@@ -2,19 +2,20 @@
 
 #here we can do It by setting a flag and taking absolute value to convert neg of a number to positive and do the calculations
 
-num=-19
-if num <0:
-    is_neg=True          #setting a neg flag
-    num=abs(num)         #taking absolute of the neg number for smooth calculations
-else:
-        neg_flag=False   #if the number is>0 then the negative flag will be False for this question
-result=' '              #saving the result in an empty string for smooth calculations 
-if num==0:
-     result=0
-while num>0:
-     result=str(num%2)+result   #last binary bit take the remainder of number by by 2(num%2=remainder)
-     num==num//2               #If we then divide the integer by 2 (num//2) now take the quotient,then all the bits gets shifted to the right keep doing successive divisionsand take each quotients as the next bits untill the quotient becomes less than 1
-if neg_flag:
-    result='-'+result           #after all the calculations we are adding the negative sign along with the result
-    print(result)
-           
+num=-19            #number is saved in a variable called num
+result=' '         #the last result we want to obtain is saved as an empty string
+if num <0:         #here if the number is less than zero that is if number is neg nummber the flag we are setting will be true
+    is_neg=True    #here flag value is_neg(flag name) is true for negative number
+    num=abs(num)   #first we are doing calculations by taking the absolute value of the negative number for the smooth calculations and It is at the last step we are adding a neg sign to the result
+else:              #else the flag value will be false
+    is_neg=False   #flag value will false
+if num==0:         #If the number is equal to zero result will be zero      
+    result=0       #the result will be zero
+while num>0:                  #here we are iterating using while loop when the number is greater than zero that is her we are taking positive part of the number for smooth calculation then after calculation we are adding negative sign
+    result=str(num%2)+result  #to get the last binary bit divide the number by 2(num%2)
+    num=num//2                #If we then divide the integer by 2 (num//2) now take the quotient,then all the bits gets shifted to the right keep doing successive divisionsand take each quotients as the next bits untill the quotient becomes less than           
+if is_neg:                    #here in this problem we have already true neg flag value so for this value,
+    result="-"+result         #we had done calculations by taking the positive value of the number so at last before printing the result we are adding a neg sign to the answer since the number here belongs to negative so the result should also neg
+    print(result)             #here atlast we are printing the result
+        
+  
