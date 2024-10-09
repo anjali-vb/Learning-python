c68e2b316de1d0e7fa6b4436ccd2520ae7f21120
Anjali V B <anjalivbbala@gmail.com>
2024-10-09 17:43:14 +0530

    New files are added

--- session6questions/cuberoot_bisection.py
+++ session6questions/cuberoot_bisection.py
@@ -4,8 +4,8 @@ cube=27                #saving the number in a variable
 epsilon=0.01           #selecting a boundary range to know how close we are to answer       
 low=0                  #here two endpoints are set for easy calculation            
 high=cube              
-guess=(low+high)/2     #midpoint is founded
-while abs(guess**3-cube)>epsilon:    #looing conditiion is made- epsilon ie guess**2-x>epsion we had set
+guess=(low+high)/2     #midpoint is found
+while abs(guess**3-cube)>epsilon:    #looping conditiion is made- epsilon ie guess**2-x>epsion we had set
  if guess**3<cube:           #if guess**3<cube, then guess will bw in low range
     low=guess
  else:                        #if guess**3<cube, then guess will bw in low range
